448a4d055a8f31da6103a822ea65f6cf53ba2153
martivo <martin.vool@cloud.ee>
2019-02-24 14:06:08 +0200

    Add code comments

--- iptables_forward.sh
+++ iptables_forward.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+#Read the current IP, feel free to change the file location.
 curip=`cat /home/centos/current_ip`
 
+#Read the current PREROUTING rules to detect the current IP that traffic is being directed to.
 chainrule=`iptables -L PREROUTING -t nat | grep "to:" | tail -1 | cut -d":" -f2`
 
+#Check if IP has changed.
 if [ "$chainrule" != "$curip" ]
 then
 echo "Updating chain due to new ip or missing rules"
 
+#Flushes all rules
 iptables -F
 iptables -t nat -F
 iptables -t mangle -F
 iptables -X
+#This is how you can "skip" redirection of some ports - in this case prometheus node exporter is still allowed into the "frontend" machine
 iptables -A PREROUTING -t nat -p tcp --dport 9100 -j ACCEPT
+#Redirecting 2222 to 22 on this host. 
 iptables -A PREROUTING -t nat -p tcp --dport 2222 -j REDIRECT --to-port 22
+#Redirect all TCP traffic to the backend server
 iptables -A PREROUTING -t nat -p tcp -j DNAT --to-destination $curip
+#Redirect all UDP traffic to the backend server
 iptables -A PREROUTING -t nat -p udp -j DNAT --to-destination $curip
+#Change the source IP of the redirected packages so they would find their way back.
 iptables -t nat -A POSTROUTING --out-interface eth0 -j MASQUERADE  
 fi
--- update_my_ip.sh
+++ update_my_ip.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-
+#Change to the actual frtonend server <user>@<ip>
 REMOTEHOST=centos@0.0.0.0
 
+#This is how your current external IP is detected, feel free to change this part if you have another way of geting your external IP.
 myip=`curl -s ifconfig.co`
 
+#elaborate check to verify that it is an IP address we got.
 if [ "`echo $myip | awk -F"." '{print NF-1}'`" != "3" ]
 then
   echo "Did not get IP. Got: $myip"
   exit 1
 fi
 
+#The last IP that was sent as an update to remote hosst.
 curip=`cat ~/current_ip`
 
 if [ "$myip" != "$curip" ]
 then
   echo "Ip changed to $myip"
+  #Copy the new IP to frontend server.
   ssh -p 2222 $REMOTEHOST "echo $myip > /home/centos/current_ip"
   if [ $? -eq 0 ]
   then
